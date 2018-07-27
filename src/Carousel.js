440c95560b2a57de57faa6e69b3282c31b4da4db
aircraftman3 <aircraftman3@gmail.com>
2018-07-27 12:26:31 +0200

    Second commit: -added files for the carousel; -edited index.html, index.js and Carousel.js;

---
+++ src/Carousel.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <img src="https://cdn.allwallpaper.in/wallpapers/1280x960/9690/singapore-skyscrapers-city-lights-petronas-towers-cities-1280x960-wallpaper.jpg" alt="City 1"/>
+      <p className="legend">City 1</p>
+    </div>
+    <div>
+      <img src="http://s1.1zoom.me/b5255/967/USA_Houses_Roads_Miami_Street_512558_1280x960.jpg" alt="City 2"/>
+      <p className="legend">City 2</p>
+    </div>
+    <div>
+      <img src="https://cdn.allwallpaper.in/wallpapers/1280x960/5051/night-ireland-dublin-cities-1280x960-wallpaper.jpg" alt="City 3"/>
+      <p className="legend">City 3</p>
+    </div>
+    <div>
+      <img src="https://cdn.allwallpaper.in/wallpapers/1280x960/603/cityscapes-night-architecture-buildings-st-louis-cities-1280x960-wallpaper.jpg" alt="City 4"/>
+      <p className="legend">City 4</p>
+    </div>
+    <div>
+      <img src="http://s1.1zoom.me/b4439/528/Germany_Berlin_Houses_Canal_Night_543381_1280x960.jpg" alt="City 5"/>
+      <p className="legend">Legend 5</p>
+    </div>
+    <div>
+      <img src="https://cdn.allwallpaper.in/wallpapers/1280x960/14441/architecture-usa-new-york-city-town-skyscrapers-cities-1280x960-wallpaper.jpg" alt="City 6"/>
+      <p className="legend">City 6</p>
+    </div>
+    <div>
+      <img src="https://www.10wallpaper.com/wallpaper/1280x960/1301/Tokyo_Tower_Japan_cities_landscape_photography_wallpaper_01_1280x960.jpg" alt="City 7"/>
+      <p className="legend">City 7</p>
+    </div>
+    <div>
+      <img src="https://www.highreshdwallpapers.com/wp-content/uploads/2014/01/Wallpaper-of-Hong-Kong-in-HD-1280x960.jpg" alt="City 8"/>
+      <p className="legend">City 8</p>
+    </div>
+    <div>
+      <img src="http://www.wallpapers13.com/wp-content/uploads/2016/01/Moscow_roads_buildings_building_city_cities_cars_night_lights-2560x1600-1280x960.jpg" alt="City 9"/>
+      <p className="legend">City 9</p>
+    </div>
+    <div>
+      <img src="https://www.10wallpaper.com/wallpaper/1280x960/1511/London_tower_bridge_thames-Cities_HD_Wallpaper_1280x960.jpg" alt="City 10"/>
+      <p className="legend">City 10</p>
+    </div>
+  </Carousel>
+);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import Carousel from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <div>
+    <Carousel />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
